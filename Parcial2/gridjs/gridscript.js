3eabfd63227bd1c1f5b510d597beef5e5983445c
Ejaredac <49420207+Ejaredac@users.noreply.github.com>
2022-03-29 14:23:42 -0500

    Grid Javascript

---
+++ Parcial2/gridjs/gridscript.js
@@ -0,0 +1,7 @@
+window.onload = function()
+	{
+	document.getElementById("idcontent").addEventListener("click",function(evento)
+	{	
+		document.getElementById(evento.target.id).classList.toggle("cajacambio");
+	});
+	}
