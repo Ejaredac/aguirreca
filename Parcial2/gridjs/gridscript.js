e5ebb950ee777e3a42ea1fbf2a2b996939c1848b
Ejaredac <49420207+Ejaredac@users.noreply.github.com>
2022-04-05 14:21:03 -0500

    Jquery trabajo

--- Parcial2/gridjs/gridscript.js
+++ Parcial2/gridjs/gridscript.js
@@ -1,7 +1,10 @@
-window.onload = function()
-	{
-	document.getElementById("idcontent").addEventListener("click",function(evento)
-	{	
-		document.getElementById(evento.target.id).classList.toggle("cajacambio");
-	});
-	}
+//window.onload = function () {
+//	document.getElementById("idcontent").addEventListener("click", function (evento) {
+//		document.getElementById(evento.target.id).classList.toggle("cajacambio");
+//	});
+//}
+$(document).ready(function(){
+	$('#idcontent').click(function(e){
+		$(e.target).toggleClass('cajaCambio');
+	})
+})
