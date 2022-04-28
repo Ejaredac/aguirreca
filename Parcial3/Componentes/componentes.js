269c73c4b5a79c05cd9dff9d706a8f2b96aeea60
Ejaredac <49420207+Ejaredac@users.noreply.github.com>
2022-04-28 14:13:43 -0500

    Componentes bootstrap jquery 28 abril

---
+++ Parcial3/Componentes/componentes.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#btn1').click(function(){
+        $('#alert1').show();
+    });
+    $('#btn2').click(function(){
+        $('#modal1').show();
+    });
+    $('#btncerrar').click(function(){
+        $('#modal1').hide();
+    });
+})
