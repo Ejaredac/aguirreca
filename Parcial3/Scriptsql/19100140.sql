b80324843f2a2c51a5a364f376d1b98ff260e64d
Ejaredac <49420207+Ejaredac@users.noreply.github.com>
2022-05-21 10:37:56 -0500

    Scriptsql

---
+++ Parcial3/Scriptsql/19100140.sql
@@ -0,0 +1,36 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema 19100140
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema 19100140
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `19100140` DEFAULT CHARACTER SET utf8 ;
+USE `19100140` ;
+
+-- -----------------------------------------------------
+-- Table `19100140`.`libro`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `19100140`.`libro` (
+  `idlibro` INT NOT NULL AUTO_INCREMENT,
+  `nombrelibro` VARCHAR(360) NULL,
+  `gusto` TEXT NULL,
+  `genero` VARCHAR(256) NULL,
+  `personajefav` VARCHAR(256) NULL,
+  `arcofav` VARCHAR(256) NULL,
+  `adult` TINYINT NULL,
+  `calificacion` INT NULL,
+  `recomendacion` TINYINT NULL,
+  PRIMARY KEY (`idlibro`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
